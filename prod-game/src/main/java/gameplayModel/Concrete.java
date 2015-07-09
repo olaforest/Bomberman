9ac7d6dfe1d5f12c561b5a6c0f0abc49efbd0a84
Olivier Laforest <olivier.laforest@mail.mcgill.ca>
2015-07-09 00:21:32 -0400

    Implemented lombok annotations for Concrete class. Fixed formatting and removed comments for Concrete class.

--- prod-game/src/main/java/gameplayModel/Concrete.java
+++ prod-game/src/main/java/gameplayModel/Concrete.java
@@ -1,34 +1,29 @@
-
 package gameplayModel;
+
+import lombok.Getter;
 
 import java.awt.image.BufferedImage;
 
-/**
- * @author Olivier Laforest
- *
- */
 public class Concrete extends GridObject {
 	
-	private BufferedImage img;
-	private int[] imageParameter = {2, 259};
+	@Getter private BufferedImage image;
+	final private int[] imageParameter = {2, 259};
 
 	public Concrete(int x, int y) {
 		
 		super(x, y);
 		
-		img = resizeImage(image.getSubimage(imageParameter[0], imageParameter[1], PIXELWIDTH, PIXELHEIGHT), ZOOM);
+		image = resizeImage(image.getSubimage(imageParameter[0], imageParameter[1], PIXELWIDTH, PIXELHEIGHT), ZOOM);
 		
 	}
 	
-	public BufferedImage getImage(){
-		return img;
-	}
-	
-    public void setXPosition(int xPosition) {
+    @Override
+	public void setXPosition(int xPosition) {
         this.xPosition = xPosition;
     }
 
-    public void setYPosition(int yPosition) {
+    @Override
+	public void setYPosition(int yPosition) {
         this.yPosition = yPosition;
     }
 }
