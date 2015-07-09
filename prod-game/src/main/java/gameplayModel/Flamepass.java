8213864ae60640ec736f8319fcbfd8e1956934eb
Olivier Laforest <olivier.laforest@mail.mcgill.ca>
2015-07-09 00:03:03 -0400

    Changed formatting and removed comments from all classes extending PowerUp class.

--- prod-game/src/main/java/gameplayModel/Flamepass.java
+++ prod-game/src/main/java/gameplayModel/Flamepass.java
@@ -1,25 +1,11 @@
-
 package gameplayModel;
-
-/**
- * This class define the the "FlamePass" PowerUp. It also contains image parameter for animation generation 
- * from Sprite Sheets
- * 
- * @author Olivier Laforest
- */
 
 public class Flamepass extends PowerUp {
 	
 	private int[] imageParam = {217, 204};
 	
-	/**
-	 * @param x the x-coordinates of location of this specific PowerUp
-	 * @param y the y-coordinates of location of this specific PowerUp
-	 */
-	
 	public Flamepass(int x, int y) {
 		super(x, y);
-		
 		isPermanent = false;
 		image = generateImage(imageParam);
 	}
--- prod-game/src/main/java/gameplayModel/Wallpass.java
+++ prod-game/src/main/java/gameplayModel/Wallpass.java
@@ -1,25 +1,11 @@
-
 package gameplayModel;
-
-/**
- * This class define the the "WallPass" PowerUp. It also contains image parameter for animation generation 
- * from Sprite Sheets
- * 
- * @author Olivier Laforest
- */
 
 public class Wallpass extends PowerUp {
 	
 	private int[] imageParam = {217, 241};
 	
-	/**
-	 * @param x the x-coordinates of location of this specific PowerUp
-	 * @param y the y-coordinates of location of this specific PowerUp
-	 */
-	
 	public Wallpass(int x, int y) {
 		super(x, y);
-		
 		isPermanent = false;
 		image = generateImage(imageParam);
 	}
