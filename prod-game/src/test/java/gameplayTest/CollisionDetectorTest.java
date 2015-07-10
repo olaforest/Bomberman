4e4ac5ed6f42e2b4279085768aaddcf4fd82fd2d
Olivier Laforest <olivier.laforest@mail.mcgill.ca>
2015-07-09 23:51:23 -0400

    Fixed errors and formatting in CollisionDetectorTest class.

--- prod-game/src/test/java/gameplayTest/CollisionDetectorTest.java
+++ prod-game/src/test/java/gameplayTest/CollisionDetectorTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gameplayTest;
 
 import gameplayModel.*;
@@ -11,245 +6,150 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Jonti
- */
 public class CollisionDetectorTest {
-    
-private static CollisionDetector detector;
 
-    @BeforeClass
-    public static void setUpClass() {
-        
-        GameContext gC = new GameContext();
-        detector = new CollisionDetector(new GameContext());
-        
-    
-    }
- 
-    /**
-     * Test of checkUpCollision method, of class CollisionDetector.
-     */
-    @Test
-    public void testCheckUpCollision() {
-        
-        GridObject a = null ;
-        GridObject b = null ;
-        
-        assertFalse(detector.checkUpCollision(a,b));
-        
-        a = new GridObject(0,100);
-        b = new GridObject(0,150);
-        
-        assertFalse(detector.checkUpCollision(a,b));
-        
-        a.xPosition = 0;
-        a.yPosition = 100;
-        
-        b.xPosition = 0;
-        b.yPosition = 80;
-                
-        assertTrue(detector.checkUpCollision(a,b));
-        
-        a.xPosition = 0;
-        a.yPosition = 100;
-        
-        b.xPosition = 0;
-        b.yPosition = 60;
-        
-        assertFalse(detector.checkUpCollision(a,b));
-        
-        
-    }
+	private static CollisionDetector detector;
 
-    /**
-     * Test of checkDownCollision method, of class CollisionDetector.
-     */
-    @Test
-    public void testCheckDownCollision() {
-        
-        GridObject a = null ;
-        GridObject b = null ;
-        
-        assertFalse(detector.checkDownCollision(a,b));
-        
-        a = new GridObject(0,100);
-        b = new GridObject(0,150);
-        
-        assertFalse(detector.checkDownCollision(a,b));
-        
-        a.xPosition = 0;
-        a.yPosition = 100;
-        
-        b.xPosition = 0;
-        b.yPosition = 80;
-                
-        assertFalse(detector.checkDownCollision(a,b));
-        
-        a.xPosition = 0;
-        a.yPosition = 100;
-        
-        b.xPosition = 0;
-        b.yPosition = 120;
-        
-        assertTrue(detector.checkDownCollision(a,b));
-         a.xPosition = 0;
-        a.yPosition = 100;
-        
-        b.xPosition = 0;
-        b.yPosition = 150;
-        
-        assertFalse(detector.checkDownCollision(a,b));
-        
-    }
+	@BeforeClass
+	public static void setUpClass() {
+		GameContext gC = new GameContext();
+		detector = new CollisionDetector(new GameContext());
+	}
 
-    /**
-     * Test of checkLeftCollision method, of class CollisionDetector.
-     */
-    @Test
-    public void testCheckLeftCollision() {
-        
-        GridObject a = null ;
-        GridObject b = null ;
-        
-        assertFalse(detector.checkLeftCollision(a,b));
-        
-        a = new GridObject(100,0);
-        b = new GridObject(150,0);
-        
-        assertFalse(detector.checkLeftCollision(a,b));
-        
-        a.xPosition = 100;
-        a.yPosition = 0;
-        
-        b.xPosition = 80;
-        b.yPosition = 0;
-                
-        assertTrue(detector.checkLeftCollision(a,b));
-        
-        a.xPosition = 100;
-        a.yPosition = 0;
-        
-        b.xPosition = 60;
-        b.yPosition = 0;
-        
-        assertFalse(detector.checkUpCollision(a,b));
-        
-        a.xPosition = 100;
-        a.yPosition = 0;
-        
-        b.xPosition = 120;
-        b.yPosition = 0;
-        
-        assertFalse(detector.checkUpCollision(a,b));
-        
-    }
+	@Test
+	public void testCheckUpCollision() {
+		GridObject a = null ;
+		GridObject b = null ;
+		assertFalse(detector.checkUpCollision(a,b));
 
-    /**
-     * Test of checkRightCollision method, of class CollisionDetector.
-     */
-    @Test
-    public void testCheckRightCollision() {
-        
-        GridObject a = null ;
-        GridObject b = null ;
-        
-        assertFalse(detector.checkLeftCollision(a,b));
-        
-        a = new GridObject(100,0);
-        b = new GridObject(150,0);
-        
-        assertFalse(detector.checkLeftCollision(a,b));
-        
-        a.xPosition = 100;
-        a.yPosition = 0;
-        
-        b.xPosition = 80;
-        b.yPosition = 0;
-                
-        assertTrue(detector.checkLeftCollision(a,b));
-        
-        a.xPosition = 100;
-        a.yPosition = 0;
-        
-        b.xPosition = 60;
-        b.yPosition = 0;
-        
-        assertFalse(detector.checkUpCollision(a,b));
-        
-        a.xPosition = 100;
-        a.yPosition = 0;
-        
-        b.xPosition = 120;
-        b.yPosition = 0;
-        
-        assertFalse(detector.checkUpCollision(a,b));
-    }
+		a = new GridObject(0,100);
+		b = new GridObject(0,150);
+		assertFalse(detector.checkUpCollision(a, b));
 
-    /**
-     * Test of checkExplGridObject method, of class CollisionDetector.
-     */
-    @Test
-    public void testCheckExplGridObject() {
-       
-    }
+//		a.xPosition = 0;
+//		a.yPosition = 100;
+//		b.xPosition = 0;
+//		b.yPosition = 80;
+		assertTrue(detector.checkUpCollision(a, b));
 
-    /**
-     * Test of checkExplEnemies method, of class CollisionDetector.
-     */
-    @Test
-    public void testCheckExplEnemies() {
-        
-    }
+//		a.xPosition = 0;
+//		a.yPosition = 100;
+//		b.xPosition = 0;
+//		b.yPosition = 60;
+		assertFalse(detector.checkUpCollision(a, b));
+	}
 
-    /**
-     * Test of checkExplBricks method, of class CollisionDetector.
-     */
-    @Test
-    public void testCheckExplBricks() {
-       Bomb bomb = new Bomb(GridObject.EFFECTIVE_PIXEL_WIDTH * 7,GridObject.EFFECTIVE_PIXEL_HEIGHT * 7);
-       
-       assertEquals((detector.checkExplBricks(bomb).indexOf(0)),-1);
-       
-       assertEquals((detector.checkExplBricks(bomb).indexOf(1)),-1);
-       
-       assertEquals((detector.checkExplBricks(bomb).indexOf(2)),-1);
-       
-       assertEquals((detector.checkExplBricks(bomb).indexOf(3)),-1);
-       
-       //int range = bomb.getRightRange();
-       
-       Brick brick = new Brick(GridObject.EFFECTIVE_PIXEL_WIDTH * 8,GridObject.EFFECTIVE_PIXEL_HEIGHT * 7);
-       
-       assertEquals((detector.checkExplBricks(bomb).indexOf(0)),-1);
-       
-       assertEquals(bomb.getRightRange(),0);
-        
-    }
+	@Test
+	public void testCheckDownCollision() {
+		GridObject a = null ;
+		GridObject b = null ;
+		assertFalse(detector.checkDownCollision(a, b));
 
-    /**
-     * Test of checkExactCollision method, of class CollisionDetector.
-     */
-    @Test
-    public void testCheckExactCollision() {
-      
-        
-        Bomberman bomberman = new Bomberman(0,0);
-        GridObject a = new GridObject(0,0);
-        
-        assertTrue(detector.checkExactCollision(bomberman, a));
-        
-        GridObject b = new GridObject(2,2);
-        
-         assertTrue(detector.checkExactCollision(bomberman, a));
-         
-           
-        Bomberman testNull = null;
-        GridObject c = null;
-        
-        assertFalse(detector.checkExactCollision(testNull, c));
-        
-    }
-    
+		a = new GridObject(0,100);
+		b = new GridObject(0,150);
+		assertFalse(detector.checkDownCollision(a, b));
+
+//		a.xPosition = 0;
+//		a.yPosition = 100;
+//		b.xPosition = 0;
+//		b.yPosition = 80;
+		assertFalse(detector.checkDownCollision(a, b));
+
+//		a.xPosition = 0;
+//		a.yPosition = 100;
+//		b.xPosition = 0;
+//		b.yPosition = 120;
+		assertTrue(detector.checkDownCollision(a, b));
+
+//		a.xPosition = 0;
+//		a.yPosition = 100;
+//		b.xPosition = 0;
+//		b.yPosition = 150;
+		assertFalse(detector.checkDownCollision(a, b));
+	}
+
+	@Test
+	public void testCheckLeftCollision() {
+		GridObject a = null ;
+		GridObject b = null ;
+		assertFalse(detector.checkLeftCollision(a,b));
+
+		a = new GridObject(100,0);
+		b = new GridObject(150,0);
+		assertFalse(detector.checkLeftCollision(a,b));
+
+//		a.xPosition = 100;
+//		a.yPosition = 0;
+//		b.xPosition = 80;
+//		b.yPosition = 0;
+		assertTrue(detector.checkLeftCollision(a,b));
+
+//		a.xPosition = 100;
+//		a.yPosition = 0;
+//		b.xPosition = 60;
+//		b.yPosition = 0;
+		assertFalse(detector.checkUpCollision(a,b));
+
+//		a.xPosition = 100;
+//		a.yPosition = 0;
+//		b.xPosition = 120;
+//		b.yPosition = 0;
+		assertFalse(detector.checkUpCollision(a,b));
+	}
+
+	@Test
+	public void testCheckRightCollision() {
+		GridObject a = null ;
+		GridObject b = null ;
+		assertFalse(detector.checkLeftCollision(a,b));
+
+		a = new GridObject(100,0);
+		b = new GridObject(150,0);
+		assertFalse(detector.checkLeftCollision(a,b));
+
+//		a.xPosition = 100;
+//		a.yPosition = 0;
+//		b.xPosition = 80;
+//		b.yPosition = 0;
+		assertTrue(detector.checkLeftCollision(a,b));
+
+//		a.xPosition = 100;
+//		a.yPosition = 0;
+//		b.xPosition = 60;
+//		b.yPosition = 0;
+		assertFalse(detector.checkUpCollision(a,b));
+
+//		a.xPosition = 100;
+//		a.yPosition = 0;
+//		b.xPosition = 120;
+//		b.yPosition = 0;
+		assertFalse(detector.checkUpCollision(a,b));
+	}
+
+	@Test
+	public void testCheckExplBricks() {
+		Bomb bomb = new Bomb(GridObject.EFFECTIVE_PIXEL_WIDTH * 7,GridObject.EFFECTIVE_PIXEL_HEIGHT * 7);
+
+		assertEquals((detector.checkExplBricks(bomb).indexOf(0)),-1);
+		assertEquals((detector.checkExplBricks(bomb).indexOf(1)),-1);
+		assertEquals((detector.checkExplBricks(bomb).indexOf(2)),-1);
+		assertEquals((detector.checkExplBricks(bomb).indexOf(3)),-1);
+
+		assertEquals((detector.checkExplBricks(bomb).indexOf(0)),-1);
+		assertEquals(bomb.getRightRange(),0);
+	}
+
+	@Test
+	public void testCheckExactCollision() {
+		Bomberman bomberman = new Bomberman(0,0);
+		GridObject a = new GridObject(0,0);
+		assertTrue(detector.checkExactCollision(bomberman, a));
+
+		GridObject b = new GridObject(2,2);
+		assertTrue(detector.checkExactCollision(bomberman, b));
+
+		Bomberman testNull = null;
+		GridObject c = null;
+		assertFalse(detector.checkExactCollision(testNull, c));
+	}
 }
