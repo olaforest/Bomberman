50ad1b5d430fd22a677dedcb1a80d7a9fee7054e
olaforest <olivier.laforest@mail.mcgill.ca>
2016-08-10 18:37:57 -0400

    Extracted the adjustment constant and added static import for MAPWIDTH in GridObject class.

--- prod-game/src/main/java/gameplayModel/GridObject.java
+++ prod-game/src/main/java/gameplayModel/GridObject.java
@@ -9,6 +9,8 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 
+import static gameplayModel.GridMap.MAPWIDTH;
+
 @Getter
 public class GridObject {
 	public static final BufferedImage sprite = loadSpriteSheet();
@@ -19,6 +21,7 @@ public class GridObject {
 	public static final int EFFECTIVE_PIXEL_WIDTH = PIXELWIDTH * ZOOM;
 	public static final int EFFECTIVE_PIXEL_HEIGHT = PIXELHEIGHT * ZOOM;
 	public static final int MISALIGNMENT_ALLOWED = 16;
+	public static final int ADJUSTMENT = 4;
 
 	protected int xPosition;
 	protected int yPosition;
@@ -32,27 +35,28 @@ public class GridObject {
 
 	public void setXPosition(int xPosition) {
 		isConcreteCollision = false;
-		boolean isInXRange = (xPosition >= EFFECTIVE_PIXEL_WIDTH) && (xPosition <= EFFECTIVE_PIXEL_WIDTH * (GridMap.MAPWIDTH - 2));
-		boolean isAlignedWithRow = ((this.yPosition - EFFECTIVE_PIXEL_HEIGHT) % (EFFECTIVE_PIXEL_HEIGHT * 2)) == 0;
-		boolean isBelowRow = ((this.yPosition - EFFECTIVE_PIXEL_HEIGHT) % (EFFECTIVE_PIXEL_HEIGHT * 2)) <= MISALIGNMENT_ALLOWED;
-		boolean isAboveRow = ((this.yPosition - EFFECTIVE_PIXEL_HEIGHT) % (EFFECTIVE_PIXEL_HEIGHT * 2)) >= (EFFECTIVE_PIXEL_HEIGHT * 2 - MISALIGNMENT_ALLOWED);
+		final int yError = (this.yPosition - EFFECTIVE_PIXEL_HEIGHT) % (EFFECTIVE_PIXEL_HEIGHT * 2);
+
+		boolean isInXRange = (xPosition >= EFFECTIVE_PIXEL_WIDTH) && (xPosition <= EFFECTIVE_PIXEL_WIDTH * (MAPWIDTH - 2));
+		boolean isAlignedWithRow = yError == 0;
+		boolean isBelowRow = yError <= MISALIGNMENT_ALLOWED;
+		boolean isAboveRow = yError >= (EFFECTIVE_PIXEL_HEIGHT * 2 - MISALIGNMENT_ALLOWED);
 
 		if (isAlignedWithRow && isInXRange) {
 			this.xPosition = xPosition;
 		} else if (isAboveRow && isInXRange) {
 			this.xPosition = xPosition;
-			this.yPosition += 4;
+			this.yPosition += ADJUSTMENT;
 		} else if (isBelowRow && isInXRange) {
 			this.xPosition = xPosition;
-			this.yPosition -= 4;
+			this.yPosition -= ADJUSTMENT;
 		} else
 			isConcreteCollision = true;
 	}
 
 	public void setYPosition(int yPosition) {
-
 		isConcreteCollision = false;
-		int xError = (this.xPosition - EFFECTIVE_PIXEL_WIDTH) % (EFFECTIVE_PIXEL_WIDTH * 2);
+		final int xError = (this.xPosition - EFFECTIVE_PIXEL_WIDTH) % (EFFECTIVE_PIXEL_WIDTH * 2);
 
 		boolean isInYRange = (yPosition >= EFFECTIVE_PIXEL_HEIGHT) && (yPosition <= EFFECTIVE_PIXEL_HEIGHT * (GridMap.MAPHEIGHT - 2));
 		boolean isAlignedWithColumn = ((xError) == 0);
@@ -63,10 +67,10 @@ public class GridObject {
 			this.yPosition = yPosition;
 		} else if (isRightFromColumn && isInYRange) {
 			this.yPosition = yPosition;
-			this.xPosition -= 4;
+			this.xPosition -= ADJUSTMENT;
 		} else if (isLeftFromColumn && isInYRange) {
 			this.yPosition = yPosition;
-			this.xPosition += 4;
+			this.xPosition += ADJUSTMENT;
 		} else
 			isConcreteCollision = true;
 	}
@@ -76,7 +80,6 @@ public class GridObject {
 	}
 
 	public static BufferedImage resizeImage(BufferedImage imageIn, int factor) {
-
 		final BufferedImage imageOut = new BufferedImage(imageIn.getWidth() * factor, imageIn.getHeight() * factor, BufferedImage.TYPE_INT_RGB);
 		final Graphics2D graphics2D = imageOut.createGraphics();
 		graphics2D.setComposite(AlphaComposite.Src);
@@ -88,7 +91,6 @@ public class GridObject {
 
 		graphics2D.drawImage(imageIn, 0, 0, imageIn.getWidth() * factor, imageIn.getHeight() * factor, null);
 		graphics2D.dispose();
-
 		return imageOut;
 	}
 
