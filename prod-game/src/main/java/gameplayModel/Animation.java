cca7a4e364955fe3dcce84980b2dbb36796486b3
Olivier Laforest <olivier.laforest@mail.mcgill.ca>
2016-08-27 11:41:33 -0400

    Converted the BufferedImage array to a list in Animation class.

--- prod-game/src/main/java/gameplayModel/Animation.java
+++ prod-game/src/main/java/gameplayModel/Animation.java
@@ -3,17 +3,19 @@ package gameplayModel;
 import lombok.Getter;
 
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Animation {
 
 	@Getter
-	private BufferedImage[] frames;
+	private List<BufferedImage> frames;
 	@Getter
 	private boolean isAnimDone;
 	private int counter;
 
 	public Animation(int frameNumber) {
-		frames = new BufferedImage[frameNumber];
+		frames = new ArrayList<>(frameNumber);
 		isAnimDone = false;
 		counter = 0;
 	}
@@ -24,22 +26,15 @@ public class Animation {
 		counter = 0;
 	}
 
-	public void setFrame(BufferedImage img, int index) {
-		frames[index] = img;
-	}
+	public void setFrame(BufferedImage img, int index) { frames.add(index, img); }
 
-	public BufferedImage getCurrentFrame() {
-		return frames[counter];
-	}
+	public BufferedImage getCurrentFrame() { return frames.get(counter); }
 
 	public void cycleFrame() {
-		counter = ((counter + 1) % frames.length);
+		counter = (counter + 1) % frames.size();
 
-		if (counter == frames.length - 1)
-			isAnimDone = true;
+		if (counter == frames.size() - 1) isAnimDone = true;
 	}
 
-	public void setToInitialFrame() {
-		counter = 0;
-	}
+	public void setToInitialFrame() { counter = 0; }
 }
