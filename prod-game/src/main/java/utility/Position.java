f408ac19e30ee1cba74dc6d214b4f054e01447a4
olaforest <olivier.laforest@mail.mcgill.ca>
2016-10-10 22:27:45 -0700

    Created Position utility class with factory methods.

--- prod-game/src/main/java/utility/Position.java
+++ prod-game/src/main/java/utility/Position.java
@@ -1,9 +1,20 @@
 package utility;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
+import static gameplayModel.GridObject.EFFECTIVE_PIXEL_DIMENSION;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Position {
 	private int x;
 	private int y;
+
+	public static Position newPosition(int x, int y) {
+		return new Position(x, y);
+	}
+
+	public static Position newModulusPosition(int x, int y) {
+		return new Position(x * EFFECTIVE_PIXEL_DIMENSION, y * EFFECTIVE_PIXEL_DIMENSION);
+	}
 }
