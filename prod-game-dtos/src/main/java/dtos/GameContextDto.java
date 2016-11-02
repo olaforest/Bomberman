cd8799bf8289f54cc546ce4582450359fb849f63
olaforest <olivier.laforest@mail.mcgill.ca>
2016-11-01 23:10:59 -0700

    Setting up jaxb2-maven-plugin.

--- prod-game-dtos/src/main/java/dtos/GameContextDto.java
+++ prod-game-dtos/src/main/java/dtos/GameContextDto.java
@@ -2,6 +2,9 @@ package dtos;
 
 import gameplayModel.GridMap;
 
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType
 public class GameContextDto {
 	private int gameTime, livesLeft, score, level, actualLevel;
 	private boolean isEndGameEnemiesSpawned;
