0966a1ead99d50f74d823da75288b326fb7f168f
homePC <olivier.laforest@mail.mcgill.ca>
2017-01-28 17:51:27 -0800

    Created unit tests for validating inputs and corresponding functionality.

--- prod-game/src/main/java/gameplayView/AnimParam.java
+++ prod-game/src/main/java/gameplayView/AnimParam.java
@@ -9,14 +9,24 @@ class AnimParam {
 	private final int numOfFrame;
 
 	AnimParam(int xCoordinate, int yCoordinate, int numOfFrame) {
-		validateCoordinate(xCoordinate, yCoordinate);
+		validateInputs(xCoordinate, yCoordinate, numOfFrame);
 		this.xCoordinate = xCoordinate;
 		this.yCoordinate = yCoordinate;
 		this.numOfFrame = numOfFrame;
 	}
 
+	private void validateInputs(int xCoordinate, int yCoordinate, int numOfFrame) {
+		validateCoordinate(xCoordinate, yCoordinate);
+		validateNumberOfFrame(numOfFrame);
+	}
+
 	private void validateCoordinate(int xCoordinate, int yCoordinate) {
 		if (xCoordinate < 0 || yCoordinate < 0)
-			throw new IllegalArgumentException("(x, y) coordinates cannot be negative");
+			throw new IllegalArgumentException("(x, y) coordinates cannot be negative.");
+	}
+
+	private void validateNumberOfFrame(int numOfFrame) {
+		if (numOfFrame < 2)
+			throw new IllegalArgumentException("Animation should have at least 2 frames.");
 	}
 }
